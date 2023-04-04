29a2c9ef2763e5b3b4f9444652d41487cb81c7d6
Véronie R. Cazeau <veronie.cazeau@gmail.com>
2023-04-04 16:10:48 -0400

    ADDED endpoint and imported tvLibrary

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import express from 'express'; 
 import cors from 'cors'; 
 
+//import CRUD functions 
+import { addNewTv, getAllTv } from './src/tvLibrary.js';
+
 const PORT = 3005;
 const app = express(); 
 
 app.use(cors()); 
 app.use(express.json()); 
+
+//get all TV shows
+app.get('/tvShows',getAllTv);
+//add TvShows
+app.post('/tvShow',addNewTv); 
+
 
 app.listen(PORT, () => {
     console.log(`Listening on http:localhost:${PORT}...`);
